e53645a68d2ad559d3c82370229a87d584cb40e8
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-02-07 15:07:33 +0100

    update 8 files

--- src/assets/Components/TextEditor/TextEditor.jsx
+++ src/assets/Components/TextEditor/TextEditor.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { db } from '../../../Firebase/firebase-config';
+import { collection, addDoc } from "firebase/firestore";
 
 const TextEditor = () => {
 
@@ -8,31 +10,45 @@ const TextEditor = () => {
 
   const modules = {
     toolbar: [
-      [{ 'header': [1, 2, false] }],
-      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-      [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
-      ['link', 'image'],
-      ['clean']
+      [{ 'header': [2, 3, false] }],
+      ['bold', 'italic', 'underline', 'strike'],
+      ['link', 'blockquote'],
+      [{ 'list': 'ordered' }, { 'list': 'bullet' }]
     ],
   }
 
   const formats = [
     'header',
-    'bold', 'italic', 'underline', 'strike', 'blockquote',
-    'list', 'bullet', 'indent',
-    'link', 'image'
+    'bold', 'italic', 'underline', 'strike',
+    'link', 'blockquote',
+    'list', 'bullet'
   ]
 
-  return <div>
-    <ReactQuill
-      theme="snow"
-      modules={modules}
-      formats={formats}
-      value={value}
-      onChange={setValue} />
-    {/* {console.log(value)} */}
-    {/* <div dangerouslySetInnerHTML={{ __html: value }} /> */}
-  </div>
+  const handleSubmit = async () => {
+    console.log('Intentando enviar a Firestore');
+    try {
+      await addDoc(collection(db, "posts"), {
+        content: value
+      });
+      console.log('Post guardado en Firestore');
+      setValue('');
+    } catch (error) {
+      console.error('Error al guardar el post:', error);
+    }
+  };
+
+  return (
+    <div>
+      <ReactQuill
+        theme="snow"
+        modules={modules}
+        formats={formats}
+        value={value}
+        onChange={setValue} />
+
+      <button onClick={handleSubmit}>Enviar</button>
+    </div>
+  )
 }
 
 export default TextEditor
