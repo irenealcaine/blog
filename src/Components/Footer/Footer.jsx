9750b4d0dbd7bead5f222d200a544b9046328b89
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-02-12 17:05:02 +0100

    update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,10 +6,10 @@ const Footer = () => {
   return (
     <nav className='footer'>
       <ul>
-        <li><a href="" rel="noopener noreferrer" target="_blank">/contact</a></li>
-        <li><a href="" rel="noopener noreferrer" target="_blank">/github</a></li>
-        <li><a href="" rel="noopener noreferrer" target="_blank">/linkedin</a></li>
-        <li><a href="" rel="noopener noreferrer" target="_blank">/portfolio</a></li>
+        <li><a href="mailto:irenealcainealvarez@gmail.com?Subject=Charlemos!" rel="noopener noreferrer" target="_blank">/contact</a></li>
+        <li><a href="https://github.com/irenealcaine" rel="noopener noreferrer" target="_blank">/github</a></li>
+        <li><a href="https://www.linkedin.com/in/irenealcaine/" rel="noopener noreferrer" target="_blank">/linkedin</a></li>
+        <li><a href="https://irenealcainealvarez.es" rel="noopener noreferrer" target="_blank">/portfolio</a></li>
         <li><Link to={'/new-post'}>/new-post</Link></li>
       </ul>
     </nav>
